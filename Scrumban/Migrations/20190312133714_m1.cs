8d322b073f9dc9ccf3a4357377bb7e9f8212947d
kogutnastua <kogut.nastua@gmail.com>
2019-04-01 16:55:00 +0300

    m

---
+++ Scrumban/Migrations/20190312133714_m1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scrumban.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    StateID = table.Column<int>(nullable: true),
+                    OwnerID = table.Column<int>(nullable: true),
+                    ReleaseID = table.Column<int>(nullable: true),
+                    Priority = table.Column<int>(nullable: true),
+                    StoryID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+        }
+    }
+}
