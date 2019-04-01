9f82517e7596ac153af9a82eb067d6178ef0a6df
kogutnastua <kogut.nastua@gmail.com>
2019-04-01 16:04:11 +0300

    Feature

--- Scrumban/Controllers/SampleDataController.cs
+++ Scrumban/Controllers/SampleDataController.cs
@@ -2,17 +2,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Scrumban.BusinessLogicLayer;
+using Scrumban.DataAccessLayer;
+using Scrumban.DataAccessLayer.Repositories;
+using Scrumban.Models;
 
 namespace Scrumban.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+        private IFeatureService featureService;
+
+        public SampleDataController (ScrumbanContext context)
+        {
+            featureService = new FeatureService(context);
+           
+        }
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<FeatureDTO> Get()
+        {
+            return featureService.Get();
+         
+        }
+
+        [HttpDelete]
+        public void Delete ([FromBody] FeatureDTO _feature)
+        {
+            featureService.Delete(_feature);
+           
+        }
+        [HttpPut]
+        public void Put([FromBody] FeatureDTO feature)
+        {
+            featureService.Put(feature);
+           
+        }
+
+        [HttpPost]
+        public void Post([FromBody]FeatureDTO feature)
+        {
+            featureService.Post(feature);
+          
+
+        }
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+      //  private object repository;
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
